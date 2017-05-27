f058d51054125a6edc3f82133815d08f3fe358b5
JavidTeam <javidteam1@gmail.com>
2017-05-27 02:29:45 -0700

    Add files via upload

---
+++ plugins/cleanbot.lua
@@ -0,0 +1,27 @@
+--Start By @ALI_K_1999
+
+local function run(msg, matches)
+if matches[1] == 'cleanbot' or 'Å«ò ò—œ‰ —»« Â«' then
+  function clbot(arg, data)
+    for k, v in pairs(data.members_) do
+      kick_user(v.user_id_, msg.to.id)
+	end
+    tdcli.sendMessage(msg.to.id, msg.id, 1, '_All Bots was cleared._', 1, 'md')
+  end
+  tdcli.getChannelMembers(msg.to.id, 0, 'Bots', 200, clbot, nil)
+  end
+end
+
+return { 
+
+patterns ={ 
+
+'^[!/#](cleanbot)$'
+ 
+ },
+  run = run
+}
+ 
+ --End By @Ali_K_1999
+ -- Channel @Javid_Team
+ --Please Not Clean This Text 
